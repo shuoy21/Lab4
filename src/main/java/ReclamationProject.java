848b462d77a9dd87c64cdfa9e2235e169b755dd8
shuoyan <shuoyan@wirelessprv-10-192-190-1.near.illinois.edu>
2017-09-26 10:41:27 -0500

    fixed reclamationProject

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -8,19 +8,45 @@
  * 5. Remove unnecessary comments as appropriate
  */
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+    /**@param
+     * this class returns the longest common substring of two strings
+     */
+public class ReclamationProject {
+    /**
+     * @param first gives one string
+     * @param second gives another string
+     * @return the longest common substring
+     */
+    static String doit(final String first, final String second) {
+        String newFirst = "";
+        String newSecond = "";
+        if (first.length() > second.length()) {
+            String holdFirst = first; // TO DO: set c to a
+            newFirst = second;
+            newSecond = holdFirst;
+            } // make sure a is a shorter string
+        //String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+        String r = "";
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+        /*for (int i = 0; i < newFirst.length(); i++) { // first: [0, length)
+            for (int j = newFirst.length() - i; j > 0; j--) { // first: (length, 0]
+                for (int k = 0; k < newSecond.length() - j; k++) { //second: [0, length)
+                    if (newFirst.regionMatches(i, newSecond, k, j)) {
+                        r = newFirst.substring(i, i + j); // Do it!
+                    }
+                        }
+                } // Ah yeah
+        }
+        */
+        for (int i = 0; i < newFirst.length(); i++) {
+            String possibleCommon = newFirst.substring(i);
+            if (newSecond.contains(possibleCommon)) {
+                r = possibleCommon;
+                break;
+            }
+        }
+        return r;
+        }
 }
